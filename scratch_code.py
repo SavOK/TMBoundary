295dfcdd044f75845beab041f9798ab8113e062d
Saveliy Belkin <Saveliy@sbelkin.com>
2019-05-30 12:16:20 -0500

    init commit

---
+++ scratch_code.py
@@ -0,0 +1,9 @@
+from optparse import OptionParser
+
+options_parser = OptionParser()
+options_parser.add_option("-in", "--input", dest="input_xml_filename",
+        help="input blast_summ.xml file")
+
+
+
+
