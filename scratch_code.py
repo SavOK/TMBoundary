61c80d8ddbcd686b6cb62c7a96fe0522b93d05c3
Saveliy Belkin <Saveliy@sbelkin.com>
2019-06-02 16:54:45 -0500

    added sql

--- scratch_code.py
+++ scratch_code.py
@@ -1,11 +1,12 @@
 from optparse import OptionParser, OptionValueError
-
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import sys
 
-from TMBoundrary import XMLParser
+import psycopg2
+import psycopg2.extras
 
+from TMBoundrary import XMLParser
 
 def _check_inputFile(option, opt_str, value, parser):
     f_path = Path(value)
@@ -14,6 +15,41 @@ def _check_inputFile(option, opt_str, value, parser):
     setattr(parser.values, option.dest, Path(value))
     parser.values.saved_infile = True
 
+conn_param_dict = {
+        "dbname":"ecod", 
+        "user":"ecodweb", 
+        "host":"feronia", 
+        "port":"45000", 
+        "password":"serveruse421",
+        }
+#conn = psycopg2.connect(**conn_param_dict)
+#cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+def get_psql_cursor(**kwark):
+    conn = psycopg2.connect(**kwark)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    return cursor
+
+def get_domain_row(domain_id:str, c):
+    c.execute('SELECT * FROM domain where id=%s', (domain_id,))
+    results = cursor.fetchone()
+    if results is None:
+        print(f"cannot find {domain_id}")
+    res_dict = {k:v for k,v in results.items()}
+    return res_dict
+
+
+def get_path_to_domain(domain_uid:str, domain_root:Path=None):
+    def _fill_uid(uid:str):
+        str_id = '0'*(9-len(uid))+uid
+        
+    if domain_root is None:
+        domain_root=Path("/data/ecod/domain_data")
+    str_id = _fill_uid(domain_uid)
+    dirpath = domain_root/str_id[2:7]/str_id
+    return dirpath
+
+    
 # if __name__ == "__main__":
 args = ['-i', './test_data/5y6p_bL.develop201.blast_summ.xml']
 options_parser = OptionParser()
@@ -26,3 +62,8 @@ if options.input_xml_filepath is None:
     sys.exit()
 
 XML_Info = XMLParser(options.input_xml_filepath)
+test = XML_Info.hh_run['hits'][5]['domain_id']
+
+
+cur = get_psql_cursor(**conn_param_dict)
+row = get_domain_row(test, cur)
