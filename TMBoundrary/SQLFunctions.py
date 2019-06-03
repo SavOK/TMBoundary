f01b17bebc0714c5335352a6bab658e7e6f796b8
Saveliy Belkin <saveliy@sbelkin.com>
2019-06-03 14:53:40 -0500

    format

--- TMBoundrary/SQLFunctions.py
+++ TMBoundrary/SQLFunctions.py
@@ -3,42 +3,43 @@ import psycopg2.extras
 
 
 class RowSQL:
-    _conn_param_dict_def = {
+    _conn_param_default = {
         "dbname": "ecod",
         "user": "ecodweb",
         "host": "129.112.32.63",
         "port": "45000",
         "password": "serveruse421",
     }
+
     def __init__(self, dbname: str = None, user: str = None,
                  host: str = None, port: str = None, password: str = None):
         self._conn_param_dict = dict()
-        print(self._conn_param_dict_def.keys())
-        if not dbname is  None:
+        if not dbname is None:
             self._conn_param_dict["dbname"] = dbname
         else:
-            self._conn_param_dict["dbname"] = self._conn_param_dict_def['dbname']
-        if not user is  None:
+            self._conn_param_dict["dbname"] = self._conn_param_default['dbname']
+        if not user is None:
             self._conn_param_dict["user"] = user
         else:
-            self._conn_param_dict["user"] = self._conn_param_dict_def['user']
-        if not host is  None:
+            self._conn_param_dict["user"] = self._conn_param_default['user']
+        if not host is None:
             self._conn_param_dict["host"] = host
         else:
-            self._conn_param_dict["host"] = self._conn_param_dict['host']
-        if   not port is  None:
+            self._conn_param_dict["host"] = self._conn_param_default['host']
+        if not port is None:
             self._conn_param_dict["port"] = port
         else:
-            self._conn_param_dict["port"] = self._conn_param_dict['port']
-        if not password is  None:
+            self._conn_param_dict["port"] = self._conn_param_default['port']
+        if not password is None:
             self._conn_param_dict["password"] = password
         else:
-            self._conn_param_dict["password"] = self._conn_param_dict['password']
+            self._conn_param_dict["password"] = self._conn_param_default['password']
+        self._connect()
 
     def _connect(self):
         self._conn = psycopg2.connect(**self._conn_param_dict)
 
-    def get_domain_row(self,domain_id: str):
+    def get_domain_row(self, domain_id: str):
         cur = self._conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute('SELECT * FROM domain where id=%s', (domain_id,))
         results = cur.fetchone()
--- scratch_code.py
+++ scratch_code.py
@@ -7,6 +7,7 @@ import sys
 from TMBoundrary import XMLParser
 from TMBoundrary import RowSQL
 
+
 def _check_inputFile(option, opt_str, value, parser):
     f_path = Path(value)
     if not f_path.is_file():
@@ -15,25 +16,25 @@ def _check_inputFile(option, opt_str, value, parser):
     parser.values.saved_infile = True
 
 
-
-def get_path_to_domain(domain_uid:str, domain_root:Path=None):
-    def _fill_uid(uid:str):
+def get_path_to_domain(domain_uid: str, domain_root: Path = None):
+    def _fill_uid(uid: str):
         return '0'*(9-len(uid))+uid
-        
+
     if domain_root is None:
-        domain_root=Path("/data/ecod/domain_data")
+        domain_root = Path("/data/ecod/domain_data")
     domain_uid = str(domain_uid)
     str_id = _fill_uid(domain_uid)
     dirpath = domain_root/str_id[2:7]/str_id
     return dirpath
 
-    
+
+
 # if __name__ == "__main__":
 args = ['-i', './test_data/5y6p_bL.develop201.blast_summ.xml']
 options_parser = OptionParser()
 options_parser.add_option("-i", "--input", dest="input_xml_filepath", type='str',
-                            help="input blast_summ.xml FILE", metavar="FILE",
-                            action='callback', callback=_check_inputFile)
+                          help="input blast_summ.xml FILE", metavar="FILE",
+                          action='callback', callback=_check_inputFile)
 (options, args) = options_parser.parse_args(args)
 if options.input_xml_filepath is None:
     options_parser.print_help()
@@ -47,4 +48,7 @@ sql = RowSQL()
 row1 = sql.get_domain_row(test1)
 row2 = sql.get_domain_row(test2)
 
-print(row1, row2)
+print(row1)
+domain_path1 = get_path_to_domain(row1['uid'])
+print(row2)
+domain_path2 = get_path_to_domain(row2['uid'])
