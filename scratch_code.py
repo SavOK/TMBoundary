bb045b828fc3a4f41303f2a0fe7b7cd87ffb5e56
SavOK <SavOK@users.noreply.github.com>
2019-06-03 18:11:18 -0500

    test

--- scratch_code.py
+++ scratch_code.py
@@ -3,15 +3,15 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 import sys
 
-
 from TMBoundrary import XMLParser
 from TMBoundrary import RowSQL
 
 
+
 def _check_inputFile(option, opt_str, value, parser):
     f_path = Path(value)
-    if not f_path.is_file():
-        raise OptionValueError(f"Cannot read input file {f_path.absolute()}")
+    if not f_path.exists():
+        raise OptionValueError(f"Cannot get {f_path()} file/directory")
     setattr(parser.values, option.dest, Path(value))
     parser.values.saved_infile = True
 
@@ -30,10 +30,13 @@ def get_path_to_domain(domain_uid: str, domain_root: Path = None):
 
 
 # if __name__ == "__main__":
-args = ['-i', './test_data/5y6p_bL.develop201.blast_summ.xml']
+args = ['-i', './test_data/5y6p_bL.develop201.blast_summ.xml', '-w', '~/Projects/TMBoundrary']
 options_parser = OptionParser()
 options_parser.add_option("-i", "--input", dest="input_xml_filepath", type='str',
                           help="input blast_summ.xml FILE", metavar="FILE",
+                          action='callback', callback=_check_inputFile)
+options_parser.add_option("-w", "--work_dir", dest="work_dir", type='str',
+                          help="DIR where structure files will be stored $DIR/TMfiles", metavar="DIR",
                           action='callback', callback=_check_inputFile)
 (options, args) = options_parser.parse_args(args)
 if options.input_xml_filepath is None:
@@ -47,10 +50,6 @@ test2 = XML_Info.hh_run['hits'][3]['domain_id']
 sql = RowSQL()
 row1 = sql.get_domain_row(test1)
 row2 = sql.get_domain_row(test2)
-
-domain_path1 = get_path_to_domain(row1['uid'])
-domain_path2 = get_path_to_domain(row2['uid'])
-
 
 def get_domain_structure_path(uid: str):
     pathtostructure = get_path_to_domain(uid)
@@ -70,5 +69,4 @@ def get_domain_structure_path(uid: str):
     print('cannot find pdb file for domain {uid}')
     return None
 
-
-pdb_list = [F for F in domain_path1.glob('*pdb') if F.is_file()]
+domain_pdb_path=get_domain_structure_path(row1['uid'])
