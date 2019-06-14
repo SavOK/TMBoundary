100063b79baccb16fe433176a36b2be7f436a739
Saveliy Belkin <Saveliy@sbelkin.com>
2019-06-14 10:18:51 -0500

    small fix

--- run_tmalign.py
+++ run_tmalign.py
@@ -98,6 +98,29 @@ def _set_output_files(infile: Path):
     new_name = f"{p[0]}.{p[1]}.blast_summ_tm.xml"
     return DIR / new_name
 
+def _get_region_from_align(align:list, region_map:dict):
+    align_res = []
+    for segment in align:
+        for r in set(range(segment[0], segment[1]+1)):
+            if r in region_map:
+                align_res.append(region_map[r])
+    
+    regions = []
+    curr_start=align_res[0]
+    curr_end = align_res[0]
+    for r in align_res[1:]:
+        if r - curr_end == 1:
+            curr_end = r
+        else:
+            if curr_end==curr_start:
+                regions.append(f"{curr_end}")
+            else:
+                regions.append(f"{curr_start}-{curr_end}")
+                curr_start=r
+                curr_end=r
+    regions.append(f"{curr_start}-{curr_end}")
+    return regions
+
 
 def _process_chain_blast(hit: dict, WD: Path, query_structure: Path):
     query_parser = PDBParser(query_structure)
@@ -142,78 +165,38 @@ def _process_chain_blast(hit: dict, WD: Path, query_structure: Path):
     return(clean_data)
 
 
-def _process_domain_blast(hit: dict, WD: Path, query_structure: Path):
+
+
+def _process_domain(hit: dict, WD: Path, query_structure: Path):
     query_parser = PDBParser(query_structure)
     query_region = _process_range(hit['query_reg'])
     query_reg_filename = f"{query_structure.stem}_{hit['query_reg']}.pdb"
     query_region_file = WD / query_reg_filename
-    query_map = query_parser.get_region(out_file=query_region_file, regions=query_region)
-
+    query_map = query_parser.get_region(
+        out_file=query_region_file, regions=query_region)
+    # domain
     sql = RowSQL()
     domain_info = sql.get_domain_row(hit['domain_id'])
     domain = Domain()
     domain_path = domain.get_structure_path(domain_info['uid'])
-
+    # run tm align
     TM = TMalign()
     TM_data = TM.run_align(query_region_file, domain_path)
-    ali_reg = TM_data['ali_reg']
-    query_dif = ali_reg[0][0] - query_region[0][0]
-    query_reg = ''
-    for r in ali_reg:
-        query_reg += f'{r[0]-query_dif}-{r[1]-query_dif},'
-    query_reg = query_reg.strip(',')
-    hit_dif = ali_reg[0][0]-1
-    hit_reg = ''
-    for r in ali_reg:
-        hit_reg += f'{r[0]-hit_dif}-{r[1]-hit_dif},'
-    hit_reg = hit_reg.strip(',')
+    query_ali = TM_data['query_reg'] 
+    query_ali_region = _get_region_from_align(query_ali, query_map)
+    hit_ali = TM_data['hit_reg'] 
+    hit_map = {k:v for k,v in 
+            zip(range(1,len(TM_data['hit_seq'])), 
+                range(1,len(TM_data['hit_seq'])))}
+    hit_ali_region = _get_region_from_align(hit_ali, hit_map)
     clean_data = {}
     clean_data['domain_id'] = hit['domain_id']
     clean_data['domain_uid'] = domain_info['uid']
     clean_data['tm_score_norm'] = TM_data['tmN']
     clean_data['tm_score_query'] = TM_data['tm1']
     clean_data['tm_score_hit'] = TM_data['tm2']
-    clean_data['query_reg'] = query_reg
-    clean_data['hit_reg'] = hit_reg
-    clean_data['query_seq'] = TM_data['query_seq']
-    clean_data['hit_seq'] = TM_data['hit_seq']
-    clean_data['tm_align'] = TM_data['tm_align']
-    return(clean_data)
-
-
-def _process_domain_hh(hit: dict, WD: Path, query_structure: Path):
-    query_parser = PDBParser(query_structure)
-    query_region = _process_range(hit['query_reg'])
-    query_reg_filename = f"{query_structure.stem}_{hit['query_reg']}.pdb"
-    query_region_file = WD / query_reg_filename
-    query_parser.get_region(out_file=query_region_file, regions=query_region)
-
-    sql = RowSQL()
-    domain_info = sql.get_domain_row(hit['domain_id'])
-    domain = Domain()
-    domain_path = domain.get_structure_path(domain_info['uid'])
-
-    TM = TMalign()
-    TM_data = TM.run_align(query_region_file, domain_path)
-    ali_reg = TM_data['ali_reg']
-    query_dif = ali_reg[0][0] - query_region[0][0]
-    query_reg = ''
-    for r in ali_reg:
-        query_reg += f'{r[0]-query_dif}-{r[1]-query_dif},'
-    query_reg = query_reg.strip(',')
-    hit_dif = ali_reg[0][0]-1
-    hit_reg = ''
-    for r in ali_reg:
-        hit_reg += f'{r[0]-hit_dif}-{r[1]-hit_dif},'
-    hit_reg = hit_reg.strip(',')
-    clean_data = {}
-    clean_data['domain_id'] = hit['domain_id']
-    clean_data['domain_uid'] = domain_info['uid']
-    clean_data['tm_score_norm'] = TM_data['tmN']
-    clean_data['tm_score_query'] = TM_data['tm1']
-    clean_data['tm_score_hit'] = TM_data['tm2']
-    clean_data['query_reg'] = query_reg
-    clean_data['hit_reg'] = hit_reg
+    clean_data['query_reg'] = ','.join(query_ali_region)
+    clean_data['hit_reg'] = ','.join(hit_ali_region)
     clean_data['query_seq'] = TM_data['query_seq']
     clean_data['hit_seq'] = TM_data['hit_seq']
     clean_data['tm_align'] = TM_data['tm_align']
@@ -240,6 +223,31 @@ def _create_domain_xml(hit, num):
     hit_seq.text = hit['hit_seq']
     return hit_xml
 
+def add_xml_hit(head:ET.Element, ix:int, I:dict):
+    ET.SubElement(head, 'hit')
+    hit = head[-1]
+    hit.attrib['num'] = str(ix+1)
+    hit.attrib['domain_id'] = str(I['domain_id'])
+    hit.attrib['domain_uid'] = str(I['domain_uid'])
+    hit.attrib['tm_score_query'] = str(I['tm_score_query'])
+    hit.attrib['tm_score_hit'] = str(I['tm_score_hit'])
+    hit.attrib['tm_score_norm'] = str(I['tm_score_norm'])
+    ET.SubElement(hit, 'query_reg')
+    query_reg = hit.find('query_reg')
+    query_reg.text = str(I['query_reg'])
+    ET.SubElement(hit, 'hit_reg')
+    hit_reg = hit.find('hit_reg')
+    hit_reg.text = str(I['hit_reg'])
+    ET.SubElement(hit, 'query_seq')
+    query_seq = hit.find('query_seq')
+    query_seq.text = I['query_seq']
+    ET.SubElement(hit, 'align_seq')
+    ali_seq = hit.find('align_seq')
+    ali_seq.text = str(I['tm_align'])
+    ET.SubElement(hit, 'domai_seq')
+    hit_seq = hit.find('domai_seq')
+    hit_seq.text = str(I['hit_seq'])
+
 
 def create_XML(old, new, Info):
     xml = ET.parse(str(old))
@@ -249,55 +257,11 @@ def create_XML(old, new, Info):
     ET.SubElement(tm, 'tm_blast_domain')
     tm_blast_domain = tm.find('tm_blast_domain')
     for ix, I in enumerate(Info['blast_domain']):
-        ET.SubElement(tm_blast_domain, 'hit')
-        hit = tm_blast_domain[ix]
-        hit.attrib['num'] = str(ix+1)
-        hit.attrib['domain_id'] = str(I['domain_id'])
-        hit.attrib['domain_uid'] = str(I['domain_uid'])
-        hit.attrib['tm_score_query'] = str(I['tm_score_query'])
-        hit.attrib['tm_score_hit'] = str(I['tm_score_hit'])
-        hit.attrib['tm_score_norm'] = str(I['tm_score_norm'])
-        ET.SubElement(hit, 'query_reg')
-        query_reg = hit.find('query_reg')
-        query_reg.text = str(I['query_reg'])
-        ET.SubElement(hit, 'hit_reg')
-        hit_reg = hit.find('hit_reg')
-        hit_reg.text = str(I['hit_reg'])
-        ET.SubElement(hit, 'query_seq')
-        query_seq = hit.find('query_seq')
-        query_seq.text = I['query_seq']
-        ET.SubElement(hit, 'align_seq')
-        ali_seq = hit.find('align_seq')
-        ali_seq.text = str(I['tm_align'])
-        ET.SubElement(hit, 'domai_seq')
-        hit_seq = hit.find('domai_seq')
-        hit_seq.text = str(I['hit_seq'])
+        add_xml_hit(tm_blast_domain, ix, I)
     ET.SubElement(tm, 'tm_hh_domain')
     tm_hh_domain = tm.find('tm_hh_domain')
     for ix, I in enumerate(Info['hh_domain']):
-        ET.SubElement(tm_hh_domain, 'hit')
-        hit = tm_hh_domain[ix]
-        hit.attrib['num'] = str(ix+1)
-        hit.attrib['domain_id'] = str(I['domain_id'])
-        hit.attrib['domain_uid'] = str(I['domain_uid'])
-        hit.attrib['tm_score_query'] = str(I['tm_score_query'])
-        hit.attrib['tm_score_hit'] = str(I['tm_score_hit'])
-        hit.attrib['tm_score_norm'] = str(I['tm_score_norm'])
-        ET.SubElement(hit, 'query_reg')
-        query_reg = hit.find('query_reg')
-        query_reg.text = str(I['query_reg'])
-        ET.SubElement(hit, 'hit_reg')
-        hit_reg = hit.find('hit_reg')
-        hit_reg.text = str(I['hit_reg'])
-        ET.SubElement(hit, 'query_seq')
-        query_seq = hit.find('query_seq')
-        query_seq.text = I['query_seq']
-        ET.SubElement(hit, 'align_seq')
-        ali_seq = hit.find('align_seq')
-        ali_seq.text = str(I['tm_align'])
-        ET.SubElement(hit, 'domai_seq')
-        hit_seq = hit.find('domai_seq')
-        hit_seq.text = str(I['hit_seq'])
+        add_xml_hit(tm_hh_domain, ix, I)
     indent(root)
     xml.write(str(new))
 
@@ -338,11 +302,11 @@ if __name__ == "__main__":
     Info['blast_domain'] = []
     for hit in XML_Info.domain_blast['hits']:
         Info['blast_domain'].append(
-            _process_domain_blast(hit, str_dir, query_structure))
+            _process_domain(hit, str_dir, query_structure))
 
     Info['hh_domain'] = []
     for hit in XML_Info.hh_run['hits']:
-        Info['hh_domain'].append(_process_domain_hh(hit, str_dir, query_structure))
+        Info['hh_domain'].append(_process_domain(hit, str_dir, query_structure))
 
     out_file = _set_output_files(options.input_xml_filepath)
     create_XML(options.input_xml_filepath, out_file, Info)
